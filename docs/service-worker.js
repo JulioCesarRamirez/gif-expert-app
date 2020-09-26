4e86643397bfa83d329fd2aee450a6247a02946c
Julio Cesar Ramirez Jimenez <julio_marshal@hotmail.com>
2020-09-25 20:18:48 -0500

    index updated

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/JulioCesarRamirez/gif-expert-app/precache-manifest.3301de7ce2558761f6f470b670f7a701.js"
+  "/precache-manifest.78ae3982ef78926ca9b4052ed2f6acbd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/JulioCesarRamirez/gif-expert-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
