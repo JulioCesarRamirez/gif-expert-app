0626e0ebaa0d29d3e3fe35a09ea2c7a629d15c9b
Julio Cesar Ramirez Jimenez <julio_marshal@hotmail.com>
2020-09-25 19:40:34 -0500

    Updates

---
+++ precache-manifest.3301de7ce2558761f6f470b670f7a701.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678a320abc042e4cd00a4a259673ab00",
+    "url": "/JulioCesarRamirez/gif-expert-app/index.html"
+  },
+  {
+    "revision": "9bd2bea355648b0b800a",
+    "url": "/JulioCesarRamirez/gif-expert-app/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "8ed7dac4bcae3ea8e8d4",
+    "url": "/JulioCesarRamirez/gif-expert-app/static/css/main.319de084.chunk.css"
+  },
+  {
+    "revision": "9bd2bea355648b0b800a",
+    "url": "/JulioCesarRamirez/gif-expert-app/static/js/2.1a68ba2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JulioCesarRamirez/gif-expert-app/static/js/2.1a68ba2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed7dac4bcae3ea8e8d4",
+    "url": "/JulioCesarRamirez/gif-expert-app/static/js/main.46ae5a68.chunk.js"
+  },
+  {
+    "revision": "3c7b77b698466735e1d6",
+    "url": "/JulioCesarRamirez/gif-expert-app/static/js/runtime-main.912ea2a2.js"
+  }
+]);
